3a2f7cab5f75531bc9c6912eeb3a261ea58ffb22
obaker815 <obaker815@gmail.com>
2025-12-04 19:20:01 +0000

    removed reloading configs

--- init.lua
+++ init.lua
@@ -8,12 +8,3 @@ require("config.lazy")
 -- Load core modules
 require("set")
 require("keys")
-
-vim.api.nvim_create_user_command("ReloadConfig", function()
-    -- Clear cache for your modules
-    for _, mod in ipairs({ "set", "keys", "config.lazy" }) do
-        package.loaded[mod] = nil
-        require(mod)
-    end
-    vim.notify("Configuration reloaded!", vim.log.levels.INFO)
-end, { desc = "Reload Neovim Lua configuration" })
