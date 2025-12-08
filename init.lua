ee26d6e3eb0bd4ccf55382e992195cd1840a8746
obaker815 <obaker815@gmail.com>
2025-12-08 16:02:45 +0000

    set default terminal to powershell

--- init.lua
+++ init.lua
@@ -2,6 +2,12 @@
 vim.g.mapleader = ','
 vim.g.localmapleader = '\\'
 
+-- Set powershell as the default terminal
+vim.opt.shell = "powershell.exe"
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
+
 -- Bootstrap lazy.nvim & load plugins
 require("config.lazy")
 
